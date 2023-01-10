eff1afc05dcb0efb7b0afed808bd28a54c7da8cc
Undefined-deve1oper <kononovkirill05@gmail.com>
2023-01-10 11:51:23 +0600

    finish sidebar layout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,13 +4,15 @@ import {
     createTheme,
     CssBaseline,
     PaletteMode,
+    Stack,
     ThemeProvider
 } from "@mui/material";
 import "./styles/app.css";
 import Header from "./components/common/Header";
+import Sidebar from "./components/common/Sidebar";
 
 const App: React.FC = () => {
-    const [mode, setMode] = useState<PaletteMode>("light");
+    const [mode, setMode] = useState<PaletteMode>("dark");
 
     const darkTheme = createTheme({
         palette: {
@@ -22,8 +24,19 @@ const App: React.FC = () => {
         <>
             <CssBaseline />
             <ThemeProvider theme={darkTheme}>
-                <Box bgcolor={"background.default"} color={"text.primary"}>
+                <Box
+                    bgcolor={"background.default"}
+                    color={"text.primary"}
+                    sx={{ minHeight: "100vh" }}
+                >
                     <Header />
+                    <Stack
+                        direction="row"
+                        spacing={2}
+                        justifyContent="space-between"
+                    >
+                        <Sidebar />
+                    </Stack>
                 </Box>
             </ThemeProvider>
         </>
---
+++ src/app/components/common/Sidebar/Sidebar.tsx
@@ -0,0 +1,103 @@
+import React from "react";
+import {
+    Box,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+    Switch
+} from "@mui/material";
+import {
+    AccountBox,
+    Article,
+    Home,
+    ModeNight,
+    People,
+    Person,
+    Settings,
+    Storefront
+} from "@mui/icons-material";
+
+const Sidebar: React.FC = () => {
+    return (
+        <Box
+            bgcolor={"background.default"}
+            color={"text.primary"}
+            flex={1}
+            p={2}
+            sx={{ display: { xs: "none", sm: "block" } }}
+        >
+            <Box position="fixed">
+                <List>
+                    <ListItem disablePadding>
+                        <ListItemButton component={"a"} href="/home">
+                            <ListItemIcon>
+                                <Home />
+                            </ListItemIcon>
+                            <ListItemText primary="Home page" />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem disablePadding>
+                        <ListItemButton component={"a"} href="/home">
+                            <ListItemIcon>
+                                <Article />
+                            </ListItemIcon>
+                            <ListItemText primary="Pages" />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem disablePadding>
+                        <ListItemButton component={"a"} href="/home">
+                            <ListItemIcon>
+                                <People />
+                            </ListItemIcon>
+                            <ListItemText primary="Groups" />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem disablePadding>
+                        <ListItemButton component={"a"} href="/home">
+                            <ListItemIcon>
+                                <Storefront />
+                            </ListItemIcon>
+                            <ListItemText primary="Marketplace" />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem disablePadding>
+                        <ListItemButton component={"a"} href="/home">
+                            <ListItemIcon>
+                                <Person />
+                            </ListItemIcon>
+                            <ListItemText primary="Friends" />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem disablePadding>
+                        <ListItemButton component={"a"} href="/home">
+                            <ListItemIcon>
+                                <Settings />
+                            </ListItemIcon>
+                            <ListItemText primary="Settings" />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem disablePadding>
+                        <ListItemButton component={"a"} href="/simple-list">
+                            <ListItemIcon>
+                                <AccountBox />
+                            </ListItemIcon>
+                            <ListItemText primary="Profile" />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem disablePadding>
+                        <ListItemButton component={"a"} href="/simple-list">
+                            <ListItemIcon>
+                                <ModeNight />
+                            </ListItemIcon>
+                            <Switch />
+                        </ListItemButton>
+                    </ListItem>
+                </List>
+            </Box>
+        </Box>
+    );
+};
+
+export default Sidebar;
