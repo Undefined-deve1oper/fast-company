f9e6dffa5cecec38d14df12308f2afb179aebbe0
Undefined-deve1oper <kononovkirill05@gmail.com>
2023-01-10 10:21:47 +0600

    adding the base container to the material-ui

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+    Box,
+    createTheme,
+    CssBaseline,
+    PaletteMode,
+    ThemeProvider
+} from "@mui/material";
 
-const App = () => {
-  return (
-	<div>App</div>
-  )
-}
+const App: React.FC = () => {
+    const [mode, setMode] = useState<PaletteMode>("light");
+
+    const darkTheme = createTheme({
+        palette: {
+            mode: mode
+        }
+    });
+
+    return (
+        <>
+            <CssBaseline />
+            <ThemeProvider theme={darkTheme}>
+                <Box bgcolor={"background.default"} color={"text.primary"}>
+                    dsfdkfsjkn
+                </Box>
+            </ThemeProvider>
+        </>
+    );
+};
 
 export default App;
